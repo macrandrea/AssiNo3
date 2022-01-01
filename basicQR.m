061f119639b9b2ef81f276c1d9c1f9638667d14c
macrandrea <macri.andr@gmail.com>
2022-01-01 20:30:57 +0100

    Update basicQR.m

--- basicQR.m
+++ basicQR.m
@@ -1,6 +1,9 @@
-function E=basicQR(A,n)
-A0=A;
+function E=basicQR(A)
+n=length(A);
     for i=1:n
-        A-i
+        if A(i)>10^(-14)
+            [Q,R]=qr(A);
+            E(i)=Q*R;
+        end
     end
 end
