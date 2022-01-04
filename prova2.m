eb71d38971b4e19e9a76c04a3cfdf9be8a6d331f
macrandrea <macri.andr@gmail.com>
2022-01-04 15:06:30 +0100

    punto_1

--- prova2.m
+++ prova2.m
@@ -4,4 +4,11 @@ A=[4 3 2 1
     3 4 3 2
     2 3 4 3
     1 2 3 4];
-[i,e]=basicQRconv(A,1);
+n=14;
+N=zeros(n,1);
+for j=1:n
+[i,e]=basicQRconv(A,j);
+N(j)=i;
+end
+x=linspace(1,i,14);
+plot(N,'h--')
